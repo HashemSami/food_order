e4d76ff5fb88fedaa3b7f74570ad3082c947edcf
hashemsami <hashem.sf@gmail.com>
2024-06-16 19:24:03 +0300

    working with figma

--- lib/food_order_web/live/page_live/item.ex
+++ lib/food_order_web/live/page_live/item.ex
@@ -3,7 +3,11 @@ defmodule FoodOrderWeb.PageLive.Item do
 
   def render(assigns) do
     ~H"""
-    <div data-role="item" data-id={@id} class="shadow-lg rounded-lg p-4 mb-2 hover:bg-neutral-100">
+    <div
+      data-role="item"
+      data-id={@id}
+      class=" bg-[#EEEEEE] text-gray-900 shadow-lg rounded-lg p-4 mb-2 hover:bg-[#686D76] hover:text-white"
+    >
       <img src={~p"/images/#{@product.image_url}"} alt="" class="h-40 mb-4 mx-auto" />
       <div class="text-center">
         <.product_details name={@product.name} size={@product.size} />
@@ -17,17 +21,18 @@ defmodule FoodOrderWeb.PageLive.Item do
     ~H"""
     <h2 class="text-lg"><%= @name %></h2>
 
-    <span class="bg-neutral-200 py-1 px-4 rounded-full uppercase text-xs">
+    <span class="bg-[#dd5d3e] py-1 px-4 rounded-full uppercase text-xs text-white">
       <%= @size %>
     </span>
     """
   end
 
+  # dc8400 random
   defp product_info(assigns) do
     ~H"""
     <div class="mt-6 flex items-center justify-around">
       <span class="font-bold text-lg"><%= @price %></span>
-      <button class="border-2 py-1 px-6 rounded-full border-blue-500 text-red-500 transition hover:border-yellow-500 hover:bg-blue-500 hover:text-neutral-100">
+      <button class="border-2 py-1 px-6 rounded-full border-blue-500 text-white transition hover:border-yellow-500 hover:bg-blue-500 hover:text-neutral-100">
         <span>+</span> <span>add</span>
       </button>
     </div>
